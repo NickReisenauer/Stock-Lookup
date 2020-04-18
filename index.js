d175349d778a3f2a5f6325370f2d5e851b8cebf0
Nick Reisenauer <nickdreisenauer@gmail.com>
2020-04-18 16:31:39 -0700

    integrated axios api call with html

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-axios
-  .get(`https://financialmodelingprep.com/api/v3/company/profile/AAPL`)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+// Output
+const name = document.querySelector("#name");
+const price = document.querySelector("#price");
+const ticker = document.querySelector("#ticker");
+const link = document.querySelector("#link");
+const change = document.querySelector("#change");
+
+const outputData = (response) => {
+  if (!response.data.symbol) {
+    return (name.textContent = `Error: ${input.value.toUpperCase()} not found`);
+  }
+  name.textContent = `${response.data.profile.companyName}`;
+  price.textContent = `$${response.data.profile.price}`;
+  ticker.textContent = `${response.data.symbol}`;
+  link.setAttribute("href", `${response.data.profile.website}`);
+  change.textContent = `${response.data.profile.changesPercentage}`;
+};
+
+submit.addEventListener("click", () => {
+  const inputValue = input.value;
+  if (inputValue) {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/company/profile/${inputValue}`
+      )
+      .then(function (response) {
+        outputData(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+});
