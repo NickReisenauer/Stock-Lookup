c81aa26d616e07329dcde438641485ba6c2da364
Nick Reisenauer <nickdreisenauer@gmail.com>
2020-04-18 17:25:42 -0700

    css styles and minor error handling

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const link = document.querySelector("#link");
 const change = document.querySelector("#change");
 
 const outputData = (response) => {
+  change.classList.remove("positive");
+  change.classList.remove("negative");
   if (!response.data.symbol) {
     return (name.textContent = `Error: ${input.value.toUpperCase()} not found`);
+  }
+  if (response.data.profile.changesPercentage.includes("+")) {
+    change.classList.add("positive");
+  } else {
+    change.classList.add("negative");
   }
   name.textContent = `${response.data.profile.companyName}`;
   price.textContent = `$${response.data.profile.price}`;
   ticker.textContent = `${response.data.symbol}`;
   link.setAttribute("href", `${response.data.profile.website}`);
   change.textContent = `${response.data.profile.changesPercentage}`;
+  document.querySelector(".container").classList.remove("hidden");
 };
 
 submit.addEventListener("click", () => {
