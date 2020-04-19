b68c75782cbef6394e1ad20b72f31baf77560e7b
Nick Reisenauer <nickdreisenauer@gmail.com>
2020-04-19 14:30:14 -0700

    added error messages and uppercase input

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ const price = document.querySelector("#price");
 const ticker = document.querySelector("#ticker");
 const link = document.querySelector("#link");
 const change = document.querySelector("#change");
+const error = document.querySelector("#error");
 
 const outputData = (response) => {
   change.classList.remove("positive");
   change.classList.remove("negative");
   if (!response.data.symbol) {
-    return (name.textContent = `Error: ${input.value.toUpperCase()} not found`);
+    return errorHandler();
   }
   if (response.data.profile.changesPercentage.includes("+")) {
     change.classList.add("positive");
   } else {
     change.classList.add("negative");
   }
+  error.textContent = "";
   name.textContent = `${response.data.profile.companyName}`;
   price.textContent = `$${response.data.profile.price}`;
   ticker.textContent = `${response.data.symbol}`;
   link.setAttribute("href", `${response.data.profile.website}`);
   change.textContent = `${response.data.profile.changesPercentage}`;
   document.querySelector(".container").classList.remove("hidden");
+};
+
+const errorHandler = () => {
+  document.querySelector(".container").classList.add("hidden");
+
+  error.textContent = `Error: ${input.value.toUpperCase()} not found`;
 };
 
 submit.addEventListener("click", () => {
